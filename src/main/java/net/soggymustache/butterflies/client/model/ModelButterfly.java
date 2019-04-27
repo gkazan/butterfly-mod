04f2521f8c70534dccdf306e4f81e75e6d6f8f43
George Kazanjian <soggymustacheofficialmods@gmail.com>
2019-04-27 18:45:06 -0400

    Add new art

--- src/main/java/net/soggymustache/butterflies/client/model/ModelButterfly.java
+++ src/main/java/net/soggymustache/butterflies/client/model/ModelButterfly.java
@@ -1,7 +1,6 @@
 package net.soggymustache.butterflies.client.model;
 
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.model.ModelBat;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
@@ -20,44 +19,44 @@ public class ModelButterfly extends ModelBase {
     public ModelButterfly() {
         this.textureWidth = 64;
         this.textureHeight = 32;
+        this.body = new ModelRenderer(this, 0, 0);
+        this.body.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.body.addBox(-1.0F, -1.0F, -3.5F, 2, 2, 7, 0.0F);
+        this.setRotateAngle(body, -0.3490658503988659F, 0.0F, 0.0F);
         this.wingRight1 = new ModelRenderer(this, 0, 9);
         this.wingRight1.mirror = true;
-        this.wingRight1.setRotationPoint(-1.0F, -1.0F, 0.0F);
-        this.wingRight1.addBox(-8.0F, 0.0F, -8.0F, 8, 0, 8, 0.0F);
-        this.wingRight2 = new ModelRenderer(this, 0, 18);
-        this.wingRight2.mirror = true;
-        this.wingRight2.setRotationPoint(-1.0F, -1.0F, 0.0F);
-        this.wingRight2.addBox(-7.0F, 0.0F, 0.0F, 7, 0, 7, 0.0F);
+        this.wingRight1.setRotationPoint(-3.0F, -1.0F, 0.0F);
+        this.wingRight1.addBox(-8.0F, 0.0F, -8.0F, 10, 0, 8, 0.0F);
+        this.wingLeft2 = new ModelRenderer(this, 0, 18);
+        this.wingLeft2.setRotationPoint(1.0F, -1.0F, 0.0F);
+        this.wingLeft2.addBox(0.0F, 0.0F, 0.0F, 9, 0, 7, 0.0F);
         this.head = new ModelRenderer(this, 11, 0);
         this.head.setRotationPoint(-0.02F, -0.1F, -3.5F);
         this.head.addBox(-1.0F, -1.0F, -2.0F, 2, 2, 2, 0.0F);
         this.setRotateAngle(head, 0.3490658503988659F, 0.0F, 0.0F);
+        this.wingLeft1 = new ModelRenderer(this, 0, 9);
+        this.wingLeft1.setRotationPoint(1.0F, -1.0F, 0.0F);
+        this.wingLeft1.addBox(0.0F, 0.0F, -8.0F, 10, 0, 8, 0.0F);
+        this.wingRight2 = new ModelRenderer(this, 0, 18);
+        this.wingRight2.mirror = true;
+        this.wingRight2.setRotationPoint(-3.0F, -1.0F, 0.0F);
+        this.wingRight2.addBox(-7.0F, 0.0F, 0.0F, 9, 0, 7, 0.0F);
+        this.antannaeLeft = new ModelRenderer(this, 0, 0);
+        this.antannaeLeft.setRotationPoint(0.6F, -0.6F, -1.0F);
+        this.antannaeLeft.addBox(0.0F, -3.0F, -0.5F, 0, 3, 1, 0.0F);
+        this.setRotateAngle(antannaeLeft, 0.3490658503988659F, 0.0F, 0.3490658503988659F);
         this.antannaeRight = new ModelRenderer(this, 0, 0);
         this.antannaeRight.mirror = true;
         this.antannaeRight.setRotationPoint(-0.6F, -0.6F, -1.0F);
         this.antannaeRight.addBox(0.0F, -3.0F, -0.5F, 0, 3, 1, 0.0F);
         this.setRotateAngle(antannaeRight, 0.3490658503988659F, 0.0F, -0.3490658503988659F);
-        this.antannaeLeft = new ModelRenderer(this, 0, 0);
-        this.antannaeLeft.setRotationPoint(0.6F, -0.6F, -1.0F);
-        this.antannaeLeft.addBox(0.0F, -3.0F, -0.5F, 0, 3, 1, 0.0F);
-        this.setRotateAngle(antannaeLeft, 0.3490658503988659F, 0.0F, 0.3490658503988659F);
-        this.wingLeft1 = new ModelRenderer(this, 0, 9);
-        this.wingLeft1.setRotationPoint(1.0F, -1.0F, 0.0F);
-        this.wingLeft1.addBox(0.0F, 0.0F, -8.0F, 8, 0, 8, 0.0F);
-        this.wingLeft2 = new ModelRenderer(this, 0, 18);
-        this.wingLeft2.setRotationPoint(1.0F, -1.0F, 0.0F);
-        this.wingLeft2.addBox(0.0F, 0.0F, 0.0F, 7, 0, 7, 0.0F);
-        this.body = new ModelRenderer(this, 0, 0);
-        this.body.setRotationPoint(0.0F, 21.0F, 0.0F);
-        this.body.addBox(-1.0F, -1.0F, -3.5F, 2, 2, 7, 0.0F);
-        this.setRotateAngle(body, -0.3490658503988659F, 0.0F, 0.0F);
         this.body.addChild(this.wingRight1);
-        this.body.addChild(this.wingRight2);
-        this.body.addChild(this.head);
-        this.head.addChild(this.antannaeRight);
-        this.head.addChild(this.antannaeLeft);
-        this.body.addChild(this.wingLeft1);
         this.body.addChild(this.wingLeft2);
+        this.body.addChild(this.head);
+        this.body.addChild(this.wingLeft1);
+        this.body.addChild(this.wingRight2);
+        this.head.addChild(this.antannaeLeft);
+        this.head.addChild(this.antannaeRight);
     }
 
     @Override
